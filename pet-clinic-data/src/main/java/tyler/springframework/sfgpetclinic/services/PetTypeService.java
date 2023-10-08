af141b3f72a1edfe54022d10c956c3a21203bcf4
Tyler Pearson <tyler.a.pear@gmail.com>
2023-10-08 11:46:57 -0500

    Adds PetTypeService and Map Impl. CLoses #28

---
+++ pet-clinic-data/src/main/java/tyler/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package tyler.springframework.sfgpetclinic.services;
+
+import tyler.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
