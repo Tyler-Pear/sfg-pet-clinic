505098c54cae1dd7e69abd7e607e8615551434e6
Tyler Pearson <tyler.a.pear@gmail.com>
2023-10-08 13:24:25 -0500

    Adds Specialty Map Impl. Closes #30

---
+++ pet-clinic-data/src/main/java/tyler/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package tyler.springframework.sfgpetclinic.services;
+
+import tyler.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long>{
+
+
+
+}
