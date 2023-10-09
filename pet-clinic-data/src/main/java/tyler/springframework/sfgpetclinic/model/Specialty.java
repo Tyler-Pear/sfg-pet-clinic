9477597dd38cb607f947d425f92d2836ff32bba7
Tyler Pearson <tyler.a.pear@gmail.com>
2023-10-09 10:18:49 -0500

    Updates Vet and Specialty to JPA. Closes #36

--- pet-clinic-data/src/main/java/tyler/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/tyler/springframework/sfgpetclinic/model/Specialty.java
@@ -1,7 +1,15 @@
 package tyler.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
