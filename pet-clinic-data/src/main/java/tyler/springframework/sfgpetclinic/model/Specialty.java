d2897de582be13d74bace4983a148ee88a9374dc
Tyler Pearson <tyler.a.pear@gmail.com>
2023-10-09 14:23:03 -0500

    Adds project Lombok. Closes #50

--- pet-clinic-data/src/main/java/tyler/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/tyler/springframework/sfgpetclinic/model/Specialty.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialty")
 public class Specialty extends BaseEntity{
@@ -12,11 +18,4 @@ public class Specialty extends BaseEntity{
     @Column(name = "description")
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
